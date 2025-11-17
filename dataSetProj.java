3143d828ff6d3587930c6dbb784a9775b4b8547b
Clemi <clmilligan@greenwichacademy.org>
2025-11-17 11:32:33 -0500

    added my data set containing all the information i will need to do my project to my repo

---
+++ dataSetProj.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class dataSetProj {
+    public static void main(String[] args) {
+        
+    }
+    public static String FoodVsSeason() throws FileNotFoundException{
+        
+
+    }
+    
+}
