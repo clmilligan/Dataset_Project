025662f7c091d3e9b46d194acbd8d3a44aa49ddd
Clemi <clmilligan@greenwichacademy.org>
2025-11-17 12:37:37 -0500

    added my reflection file as a txt

--- dataSetProj.java
+++ dataSetProj.java
@@ -5,7 +5,13 @@ public class dataSetProj {
         
     }
     public static String FoodVsSeason() throws FileNotFoundException{
-        
+        File f=new File("/Users/clmilligan/HCSS/Dataset_Project/Foodpanda Analysis Dataset.csv");
+        Scanner s = new Scanner(f);
+        while(s.hasNextLine()){
+            String line =s.nextLine();
+            String[] str =line.split(",");
+
+        }
 
     }
     
