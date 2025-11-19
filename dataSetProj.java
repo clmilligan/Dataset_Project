a0df47d1c671c3a507d39410bebfa77499e24964
Clemi <clmilligan@greenwichacademy.org>
2025-11-19 10:19:37 -0500

    i checked the data set again and realized that a food type was a sandwich so I had to fix both my methods to include sandwiches as a food option

--- dataSetProj.java
+++ dataSetProj.java
@@ -7,19 +7,19 @@ public class dataSetProj {
     public static String FoodVsSeason() throws FileNotFoundException{
         File f=new File("/Users/clmilligan/HCSS/Dataset_Project/Foodpanda Analysis Dataset.csv");
         Scanner s = new Scanner(f);
-        int[] S1 = {0,0,0,0};
-        int[] S2 ={0,0,0,0};
-        int[] S3={0,0,0,0};
-        int [] S4={0,0,0,0};
+        int[] S1 = {0,0,0,0,0};//arrays to count the number of each food
+        int[] S2 ={0,0,0,0,0};
+        int[] S3={0,0,0,0,0};
+        int [] S4={0,0,0,0,0};
 
         while(s.hasNextLine()){
-            String line =s.nextLine();
+            String line =s.nextLine();//to itterate through each line of the .csv file
             String[] str =line.split(",");
             String curr=str[6];
             int cut =curr.indexOf("/");
             if(cut!=-1){
                 
-                if (Double.parseDouble(curr.substring(0,cut))<4){
+                if (Double.parseDouble(curr.substring(0,cut))<4){//checks the month of the food and then checks what food it is and tallys it in an array
                     if(str[8].equals("Burger")){
                         S1[0]=S1[0]+1;
                     }
@@ -32,6 +32,10 @@ public class dataSetProj {
                     else if(str[8].equals("Fries")){
                         S1[3]=S1[3]+1;
                     }
+                    else if(str[8].equals("Sandwich")){
+                        S1[4]=S1[4]+1;
+                    }
+
                 }
                 else if (Double.parseDouble(curr.substring(0,cut))<7){
                     if(str[8].equals("Burger")){
@@ -46,6 +50,10 @@ public class dataSetProj {
                     else if(str[8].equals("Fries")){
                         S2[3]=S1[3]+1;
                     }
+                    else if(str[8].equals("Sandwich")){
+                        S2[4]=S2[4]+1;
+                    }
+
                 }
                 else if (Double.parseDouble(curr.substring(0,cut))<10){
                     if(str[8].equals("Burger")){
@@ -59,6 +67,9 @@ public class dataSetProj {
                     }
                     else if(str[8].equals("Fries")){
                         S3[3]=S1[3]+1;
+                    }
+                    else if(str[8].equals("Sandwich")){
+                        S3[4]=S3[4]+1;
                     }
                 }
                 else if (Double.parseDouble(curr.substring(0,cut))<13){
@@ -74,6 +85,9 @@ public class dataSetProj {
                     else if(str[8].equals("Fries")){
                         S4[3]=S1[3]+1;
                     }
+                    else if(str[8].equals("Sandwich")){
+                        S4[4]=S4[4]+1;
+                    }
                 }
             
             }
@@ -81,22 +95,25 @@ public class dataSetProj {
 
 
         }
+        
         String fr=max(S1);
-            String Sc=max(S2);
-            String tr=max(S3);
-            String fo=max(S4);
+        String Sc=max(S2);
+        String tr=max(S3);
+        String fo=max(S4);
+        
 
-            return "the most popular food in the winter is "+fr+"\n the most popular food in the spring is "+Sc+"\n the most popular food in the summer is "+tr+"\n the most popular food in the fall is "+fo;
+        return "the most popular food in the winter is "+fr+"\n the most popular food in the spring is "+Sc+"\n the most popular food in the summer is "+tr+"\n the most popular food in the fall is "+fo;
 
         
 
     }
-    public static String max (int[] arr){//a method to determine the max number in of foods and 
-        int max=Math.max(Math.max(arr[0],arr[1]),Math.max(arr[2],arr[3]));
+    public static String max (int[] arr){//a method to determine the most popular food based on the array and then return the corresponding food
+        int max=Math.max(Math.max(arr[0],arr[1]),Math.max(Math.max(arr[4],arr[2]),arr[3]));
         int indx=0;
         for(int i=0; i<arr.length;i++){
             if (max==arr[i]){
                 indx=i;
+                
             }
         }
         if(indx==0){
@@ -110,6 +127,9 @@ public class dataSetProj {
         }
         else if(indx==3){
             return "Fries";
+        }
+        else if(indx==4){
+            return "Sandwich";
         }
         return "erm";
     }
